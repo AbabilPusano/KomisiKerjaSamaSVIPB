e6d63bb7e3c521b90aaddcadda53984479f4d381
AbabilPusano <ababilpusano@apps.ipb.ac.id>
2026-01-19 10:33:45 +0700

    feat: add PPBJ layout component, including metadata and a language provider.

--- app/ppbj/layout.tsx
+++ app/ppbj/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { LanguageProvider } from "../vcc/components/LanguageProvider";
-import Footer from "./components/Footer";
 import { metadata } from "./metadata";
 
 export { metadata };
@@ -15,7 +14,6 @@ export default function PPBJLayout({
 		<LanguageProvider>
 			<div className="min-h-screen bg-white flex flex-col">
 				<main className="flex-1">{children}</main>
-				<Footer />
 			</div>
 		</LanguageProvider>
 	);
