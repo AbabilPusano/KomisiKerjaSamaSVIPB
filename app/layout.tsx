f6c23bcf5989392036b92fe003097402c2560c7c
AbabilPusano <ababilpusano@apps.ipb.ac.id>
2026-01-20 13:31:31 +0700

    feat: Add root layout with metadata, global styles, and LanguageProvider.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata = {
 	title: "PPBJ - Japanese Language Training Programs",
 	description:
 		"Empowering Japanese language learning through innovative programs and expert instruction",
+	icons: {
+		icon: "/images/logo/vcc-logo-only.svg",
+	},
 };
 
 export default function RootLayout({
