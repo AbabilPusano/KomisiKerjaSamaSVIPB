14c735517618f7a5139e0be209e03cec7388bc2b
AbabilPusano <ababilpusano@apps.ipb.ac.id>
2026-01-20 13:34:36 +0700

    feat: Initialize root layout with global styles, metadata, and LanguageProvider.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import "./vcc/globals.css";
 import { LanguageProvider } from "./vcc/components/LanguageProvider";
 
 export const metadata = {
-	title: "PPBJ - Japanese Language Training Programs",
-	description:
-		"Empowering Japanese language learning through innovative programs and expert instruction",
+	title: "Vocational Colaboration Center SV IPB",
 	icons: {
 		icon: "/images/logo/vcc-logo-only.jpg",
 	},
