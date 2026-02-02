2866d6419a8961314c73bad1325e82920068fe7d
M Rafka Hadyan S <rafkahs2310@gmail.com>
2026-02-02 09:56:22 +0700

    delete Japancopy

--- app/programs/japan copy/ppbj/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-	title: "PPBJ Aceh - Pusat Pelatihan Bahasa Jepang",
-	description: "Belajar Bahasa Jepang & Siap Kerja di Jepang bersama PPBJ Aceh",
-};
